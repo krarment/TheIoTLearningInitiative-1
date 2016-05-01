ae41fba832158d89d29f6912243799a391fcd0ce
Abraham Arce <xe1gyq@gmail.com>
2016-05-01 13:23:20 +0000

    Internet of Things 101: 5.4.2 Protocols: Project: MQTT Publish
    Publish sensor data through MQTT Protocol using test.mosquitto.org
    server under "IoT101/DataSensor" topic
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
+import paho.mqtt.client as paho
 import psutil
 import signal
 import sys
 import time
+
+from threading import Thread
 
 def functionDataActuator():
     print "Data Actuator"
@@ -13,12 +16,28 @@ def functionDataSensor():
     data = netdata.packets_sent + netdata.packets_recv
     return data
 
+def functionDataSensorMqttOnPublish(mosq, obj, msg):
+    print "Data Sensor Mqtt Published!"
+
+def functionDataSensorMqttPublish():
+    mqttclient = paho.Client()
+    mqttclient.on_publish = functionDataSensorMqttOnPublish
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
+    while True:
+        data = functionDataSensor()
+        topic = "IoT101/DataSensor"
+        mqttclient.publish(topic, data)
+        time.sleep(1)
+
 def functionSignalHandler(signal, frame):
     sys.exit(0)
 
 if __name__ == '__main__':
 
     signal.signal(signal.SIGINT, functionSignalHandler)
+
+    threadmqttpublish = Thread(target=functionDataSensorMqttPublish)
+    threadmqttpublish.start()
 
     while True:
         print "Hello Internet of Things 101"
